9b8eae2f373af9de80d3af7b158e14b5fbdf0f7f
litmuss <sunsu2737@gmail.com>
2021-08-12 17:54:11 +0900

    기본적인 사용법 + counter

---
+++ begin-react/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Hello from './Hello';
+import Wrapper from './Wrapper';
+import Counter from './Counter';
+function App() {
+  return (
+    <Counter></Counter>
+  );
+}
+
+export default App;
